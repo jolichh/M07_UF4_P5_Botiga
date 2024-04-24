440b83688eedf7ad9b8cff84201dc8b5b1609305
= <=>
2024-04-24 18:26:08 +0200

    Fichero Serializers con el GET y POST en processo

---
+++ botiga_grupJ/carreto/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Carrito
+
+class CarritoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Carrito
+        fields = '__all__'
+
+class CarritoPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Carrito
+        fields = ['productos']
