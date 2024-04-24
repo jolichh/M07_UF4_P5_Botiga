da5caea91ae2e9b750462dc726bad6debc3d3749
= <=>
2024-04-24 18:24:37 +0200

    Cambio de forenKey al ManyToMany para los productos

--- botiga_grupJ/carreto/models.py
+++ botiga_grupJ/carreto/models.py
@@ -4,4 +4,5 @@ from cataleg.models import Producte
 # Create your models here.
 class Carrito(models.Model):
     id = models.AutoField(primary_key=True)
-    productos = models.ForeignKey(Producte, on_delete=models.CASCADE)
+    # Relación uno a muchos con la tabla Producte
+    productos = models.ManyToManyField(Producte)  
