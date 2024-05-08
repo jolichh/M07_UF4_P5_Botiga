200c4f0f6760a161be6e096613a269c1d2e54c26
= <=>
2024-05-08 19:02:44 +0200

    He agregado al modelo la cantidad de cada producto que hay en el carrito

--- botiga_grupJ/carreto/models.py
+++ botiga_grupJ/carreto/models.py
@@ -2,7 +2,13 @@ from django.db import models
 from cataleg.models import Producte
 
 # Create your models here.
+
 class Carrito(models.Model):
     id = models.AutoField(primary_key=True)
     # Relación uno a muchos con la tabla Producte
-    productos = models.ManyToManyField(Producte)  
+    productos = models.ManyToManyField(Producte, through='ProductoEnCarrito')
+
+class ProductoEnCarrito(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producte, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
