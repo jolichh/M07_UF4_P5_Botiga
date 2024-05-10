11bc5e7f8fcb8cd353273f50c8a701a1b43534e6
jolichh <113618956+jolichh@users.noreply.github.com>
2024-05-10 18:54:11 +0200

    update readme

--- README.md
+++ README.md
@@ -20,6 +20,31 @@ INSERT INTO cataleg_producte (name, description, price, valoration, quantity, ca
 ('Pantalón', 'Pantalón vaquero de estilo casual', 40.00, 5, 25, 2),
 ('Cafetera', 'Cafetera de cápsulas con sistema de autolimpieza', 90.00, 4, 8, 3);
 ```
-
+### Ejemplo inserción de datos en Categoria (nuevo producto)
+{
+    "name": "Producto Nuevo",
+    "description": "Descripcion de producto nuevo...",
+    "price": "200",
+    "valoration": 4,
+    "quantity": 20,
+    "categoria": 1
+}
+* Para editar sigue el mismo formato json
 **Tabla Pagament**
+Insertar datos prueba a User
+```
+INSERT INTO pagaments_user (name, email, password) VALUES
+('Usuario 1', 'usuario1@example.com', 'contraseña1'),
+('Usuario 2', 'usuario2@example.com', 'contraseña2'),
+('Usuario 3', 'usuario3@example.com', 'contraseña3');
+```
+
+Insertar datos de pagamentos
+```
+INSERT INTO tuapp_pagament (tarjet_num, exp_date, cvc, user_id) VALUES
+('1234567890123456', '2025-12-31', 123, 1),
+('9876543210987654', '2026-12-31', 456, 2),
+('1111222233334444', '2027-12-31', 789, 3);
+```
+
 
--- botiga_grupJ/pagaments/views.py
+++ botiga_grupJ/pagaments/views.py
@@ -22,57 +22,59 @@ def pagaments(request):
             return Response(status=status.HTTP_404_NOT_FOUND)
     
         
-    # elif request.method == 'POST':
-    #     # AÑADIR DATOS DE PAGO
-    #     serializer = AddPagamentSerializer(data=request.data)
-    #     # Asegurar formato de datos cumple la serialización
-    #     if serializer.is_valid():
-    #         # Verificar si ya existe un pago para este usuario
-    #         user_id = serializer.validated_data['user']
-    #         if not Pagament.objects.filter(user_id=user_id).exists():
-    #             serializer.save()
-    #             return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #         else:
-    #             return Response({"message": "Ya existe un pago para este usuario"}, status=status.HTTP_400_BAD_REQUEST)
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #    return Response({"message": "Asegurate de introducir datos y formatos validos"})
+    elif request.method == 'POST':
+        # AÑADIR DATOS DE PAGO
+        serializer = AddPagamentSerializer(data=request.data)
+        # Asegurar formato de datos cumple la serialización
+        if serializer.is_valid():
+            # Verificar si ya existe un pago para este usuario
+            user_id = serializer.validated_data['user']
+            if not Pagament.objects.filter(user_id=user_id).exists():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message": "Ya existe un pago para este usuario"}, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response({"message": "Asegurate de introducir datos y formatos validos"})
     
        
     return Response({"nada a mostrar..."})
 
-# trabaja sobre un dato especifico
-# @api_view(['GET','PUT','DELETE'])
-# def update_delete_pagament(request, pk=None):
-#     if request.method == 'GET':
-#         try: 
-#             pagament = Pagament.objects.get(pk=pk)            
-#             #Serializar pagos ordenando y mostrar datos del user
-#             serializer = GetPagamentSerializer(pagament)
+# trabaja sobre un dato especifico de pago: 
+# muestra los datos de pago de un usuario, junto a todos los datos relacionados a ese user
+@api_view(['GET','PUT','DELETE'])
+def update_delete_pagament(request, pk=None):
+    # if request.method == 'GET':
+    #     try: 
+    #         pagament = Pagament.objects.get(pk=pk)            
+    #         #Serializar pagos ordenando y mostrar datos del user
+    #         serializer = GetPagamentSerializer(pagament)
 
-#             return Response(serializer.data)
-#         except Pagament.DoesNotExist:
-#             return Response(status=status.HTTP_404_NOT_FOUND)
+    #         return Response(serializer.data)
+    #     except Pagament.DoesNotExist:
+    #         return Response(status=status.HTTP_404_NOT_FOUND)
         
-#     if request.method == 'PUT':
-#         try:
-#             pagament = Pagament.objects.get(pk=pk)
-#         except Pagament.DoesNotExist:
-#             return Response({"message": "No se ha encontrado coincidencias en la data base para ese ID"}, status=status.HTTP_404_NOT_FOUND)
+    # if request.method == 'PUT':
+    #     try:
+    #         pagament = Pagament.objects.get(pk=pk)
+    #     except Pagament.DoesNotExist:
+    #         return Response({"message": "No se ha encontrado coincidencias en la data base para ese ID"}, status=status.HTTP_404_NOT_FOUND)
 
-#         serializer = AddPagamentSerializer(pagament, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     serializer = AddPagamentSerializer(pagament, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-#     elif request.method == 'DELETE':
-#         try:
-#             pagament = Pagament.objects.get(pk=pk)
-#         except Pagament.DoesNotExist:
-#             return Response({"message": "El pago no existe"}, status=status.HTTP_404_NOT_FOUND)
+    # elif request.method == 'DELETE':
+    #     try:
+    #         pagament = Pagament.objects.get(pk=pk)
+    #     except Pagament.DoesNotExist:
+    #         return Response({"message": "El pago no existe"}, status=status.HTTP_404_NOT_FOUND)
        
-#         pagament.delete()
-#         return Response({"message": "El metodo de pago se ha eliminado correctamente"})
+    #     pagament.delete()
+    #     return Response({"message": "El metodo de pago se ha eliminado correctamente"})
     
-#     return Response({"nada a mostrar..."})
+    return Response({"nada a mostrar..."})
