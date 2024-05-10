e08255231f18637449b3a4d58a988e1827693be2
jolichh <113618956+jolichh@users.noreply.github.com>
2024-05-10 16:17:23 +0200

    dependencia de comandes

--- botiga_grupJ/pagaments/serializers.py
+++ botiga_grupJ/pagaments/serializers.py
@@ -1,32 +1,60 @@
 from django.contrib.auth.models import Group, User
+from botiga_grupJ.comandes.models import Comanda
 from .models import Pagament, User
 from rest_framework import serializers
 
+# campos del user
+class UserSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model= User
+        fields = ['id','name', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}  # No mostrar contraseña
 
-class UserPagamentSerializer(serializers.ModelSerializer):
-    #guardarà los datos de manera filtrada
-    pagaments = serializers.SerializerMethodField()
+
+# comanda con datos del user
+# cada comanda puede tener varios carritos con propiedad de pagado o no
+class ComandaSerializer(serializers.ModelSerializer):
+    user = UserSerializer()  # Incluir el serializador de User para mostrar los datos del usuario
 
     class Meta:
-        model = User
-        fields = ['id', 'name', 'email', 'password', 'pagaments']
+        model = Comanda
+        fields = ['id', 'carreto', 'data', 'user']  # Incluir los campos necesarios de Comanda
 
-    # def get_pagaments(self,obj):
-    #     pagaments = Pagament.objects.filter(user=obj.id)
-    #     return PagamentSerializer(pagaments, many=True).data
 
+# serializador de campos de la tabla pagament
+class PagamentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Pagament
+        fields = ['id','tarjet_num', 'exp_date', 'cvc', 'user']
+
+
+# para mostrar TODOS los datos al pagar: comanda, user, datos de pago
 class GetPagamentSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model=Pagament
         fields = ['id','tarjet_num', 'exp_date', 'cvc', 'user']
 
-class AddPagamentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pagament
-        fields = ['tarjet_num', 'exp_date', 'cvc', 'user']
 
-class UserSerializer(serializers.ModelSerializer):    
-    class Meta:
-        model= User
-        fields = ['name', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}  # No mostrar contraseña
+# class UserPagamentSerializer(serializers.ModelSerializer):
+#     #guardarà los datos de manera filtrada
+#     pagaments = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = User
+#         fields = ['id', 'name', 'email', 'password', 'pagaments']
+
+#     # def get_pagaments(self,obj):
+#     #     pagaments = Pagament.objects.filter(user=obj.id)
+#     #     return PagamentSerializer(pagaments, many=True).data
+
+# class GetPagamentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model=Pagament
+#         fields = ['id','tarjet_num', 'exp_date', 'cvc', 'user']
+
+# class AddPagamentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Pagament
+#         fields = ['tarjet_num', 'exp_date', 'cvc', 'user']
+
--- botiga_grupJ/pagaments/views.py
+++ botiga_grupJ/pagaments/views.py
@@ -6,7 +6,8 @@ from rest_framework import status
 from .models import Pagament
 from .serializers import AddPagamentSerializer, GetPagamentSerializer, UserPagamentSerializer, UserSerializer
 
-# Mostrar todos los datos de pago con su user_id
+# Mostrar los datos relacionados al pago: 
+# todos los productos de la comanda junto al user y sus datos de pago
 @api_view(['GET', 'POST'])
 def pagaments(request):
     if request.method == 'GET':
@@ -21,57 +22,57 @@ def pagaments(request):
             return Response(status=status.HTTP_404_NOT_FOUND)
     
         
-    elif request.method == 'POST':
-        # AÑADIR DATOS DE PAGO
-        serializer = AddPagamentSerializer(data=request.data)
-        # Asegurar formato de datos cumple la serialización
-        if serializer.is_valid():
-            # Verificar si ya existe un pago para este usuario
-            user_id = serializer.validated_data['user']
-            if not Pagament.objects.filter(user_id=user_id).exists():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response({"message": "Ya existe un pago para este usuario"}, status=status.HTTP_400_BAD_REQUEST)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response({"message": "Asegurate de introducir datos y formatos validos"})
+    # elif request.method == 'POST':
+    #     # AÑADIR DATOS DE PAGO
+    #     serializer = AddPagamentSerializer(data=request.data)
+    #     # Asegurar formato de datos cumple la serialización
+    #     if serializer.is_valid():
+    #         # Verificar si ya existe un pago para este usuario
+    #         user_id = serializer.validated_data['user']
+    #         if not Pagament.objects.filter(user_id=user_id).exists():
+    #             serializer.save()
+    #             return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #         else:
+    #             return Response({"message": "Ya existe un pago para este usuario"}, status=status.HTTP_400_BAD_REQUEST)
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #    return Response({"message": "Asegurate de introducir datos y formatos validos"})
     
        
     return Response({"nada a mostrar..."})
 
 # trabaja sobre un dato especifico
-@api_view(['GET','PUT','DELETE'])
-def update_delete_pagament(request, pk=None):
-    if request.method == 'GET':
-        try: 
-            pagament = Pagament.objects.get(pk=pk)            
-            #Serializar pagos ordenando y mostrar datos del user
-            serializer = GetPagamentSerializer(pagament)
+# @api_view(['GET','PUT','DELETE'])
+# def update_delete_pagament(request, pk=None):
+#     if request.method == 'GET':
+#         try: 
+#             pagament = Pagament.objects.get(pk=pk)            
+#             #Serializar pagos ordenando y mostrar datos del user
+#             serializer = GetPagamentSerializer(pagament)
 
-            return Response(serializer.data)
-        except Pagament.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+#             return Response(serializer.data)
+#         except Pagament.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
         
-    if request.method == 'PUT':
-        try:
-            pagament = Pagament.objects.get(pk=pk)
-        except Pagament.DoesNotExist:
-            return Response({"message": "No se ha encontrado coincidencias en la data base para ese ID"}, status=status.HTTP_404_NOT_FOUND)
+#     if request.method == 'PUT':
+#         try:
+#             pagament = Pagament.objects.get(pk=pk)
+#         except Pagament.DoesNotExist:
+#             return Response({"message": "No se ha encontrado coincidencias en la data base para ese ID"}, status=status.HTTP_404_NOT_FOUND)
 
-        serializer = AddPagamentSerializer(pagament, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         serializer = AddPagamentSerializer(pagament, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    elif request.method == 'DELETE':
-        try:
-            pagament = Pagament.objects.get(pk=pk)
-        except Pagament.DoesNotExist:
-            return Response({"message": "El pago no existe"}, status=status.HTTP_404_NOT_FOUND)
+#     elif request.method == 'DELETE':
+#         try:
+#             pagament = Pagament.objects.get(pk=pk)
+#         except Pagament.DoesNotExist:
+#             return Response({"message": "El pago no existe"}, status=status.HTTP_404_NOT_FOUND)
        
-        pagament.delete()
-        return Response({"message": "El metodo de pago se ha eliminado correctamente"})
+#         pagament.delete()
+#         return Response({"message": "El metodo de pago se ha eliminado correctamente"})
     
-    return Response({"nada a mostrar..."})
+#     return Response({"nada a mostrar..."})
