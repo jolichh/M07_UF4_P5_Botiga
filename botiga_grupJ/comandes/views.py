a1e162e9c854ea8b78b6cf528b2972ee3abffab8
= <=>
2024-05-13 16:05:29 +0200

    Intentando el GET de comandes

--- botiga_grupJ/comandes/views.py
+++ botiga_grupJ/comandes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Comanda
+from .serializers import ComandaSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def Comanda(request):
+    if request.method == 'GET':
+        comandas = Comanda.objects.all()
+        serializer = ComandaSerializer(comandas, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        return Response({'message': 'POST method not implemented'}, status=501)
