c7c38106e6c501a4c2de7f20be20d73789cbf3f7
= <=>
2024-05-14 18:26:13 +0200

    He creado los metodos GET para mostrar las comandas finalizadas y las no finalizadas

--- botiga_grupJ/comandes/views.py
+++ botiga_grupJ/comandes/views.py
@@ -8,3 +8,18 @@ def comanda_list(request):
     comandas = Comanda.objects.all()
     serializer = ComandaSerializer(comandas, many=True)
     return Response(serializer.data)
+
+#Solo muestra los carritos que han sido comprados
+@api_view(['GET'])
+def comanda_list_completed(request):
+    comandas = Comanda.objects.filter(compra_realizada=True)
+    serializer = ComandaSerializer(comandas, many=True)
+    return Response(serializer.data)
+
+
+#Solo muestra los carritos que no han sido comprados
+@api_view(['GET'])
+def comanda_list_not_completed(request):
+    comandas = Comanda.objects.filter(compra_realizada=False)
+    serializer = ComandaSerializer(comandas, many=True)
+    return Response(serializer.data)
