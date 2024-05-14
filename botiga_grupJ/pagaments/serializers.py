17e263bcdc4ef68b6fc142470202ab82d181e8f3
jolichh <113618956+jolichh@users.noreply.github.com>
2024-05-14 23:43:28 +0200

    limpiar codigo

--- botiga_grupJ/pagaments/serializers.py
+++ botiga_grupJ/pagaments/serializers.py
@@ -7,27 +7,6 @@ from .models import Pagament, User
 from rest_framework import serializers
 from django.contrib.auth.hashers import check_password
 
-# campos del user
-# class UserSerializer(serializers.ModelSerializer):    
-#     class Meta:
-#         model= User
-#         fields = ['id','name', 'email', 'password']
-#         extra_kwargs = {'password': {'write_only': True}}  # No mostrar contraseña
-
-
-# comanda con datos del carrito
-# cada comanda puede tener varios carritos con propiedad de pagado o no
-# class ComandaSerializer(serializers.ModelSerializer):
-#     carrito = serializers.SerializerMethodField()
-
-#     class Meta:
-#         model = Comanda
-#         fields = ['id', 'carreto', 'data', 'user']  # Incluir los campos necesarios de Comanda
-
-#     def get_carrito(self, obj):
-#         comandes = Carrito.objects.filter(user=obj.id)
-#         #importamos serializador de la app carreto
-#         return CarritoSerializer(comandes, many=True).data
 
 # serializador de campos de la tabla pagament
 class PagamentSerializer(serializers.ModelSerializer):
@@ -36,43 +15,6 @@ class PagamentSerializer(serializers.ModelSerializer):
         fields = ['id','tarjet_num', 'exp_date', 'cvc', 'user']
 
 
-# class GetPagamentSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=Pagament
-#         fields = ['id','tarjet_num', 'exp_date', 'cvc', 'user']
-
-
-# serializador para añadir una targeta
-class AddPagamentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pagament
-        fields = ['tarjet_num', 'exp_date', 'cvc', 'user']
-
-# muestra datos del usuario junto a sus datos de comandas
-# class UserPagamentSerializer(serializers.ModelSerializer):
-#     # guardarà los datos de manera filtrada
-#     # no es un campo real, obtiene los datos a través de las 'def'
-#     # donde obtiene los datos relacionados a cada user
-#     pagaments = serializers.SerializerMethodField()
-#     comandes = serializers.SerializerMethodField()
-
-#     class Meta:
-#         model = User
-#         fields = ['id', 'name', 'email', 'password', 'pagaments', 'comandes']
-
-#     def get_pagaments(self,obj):
-#         pagaments = Pagament.objects.filter(user=obj.id)
-#         return PagamentSerializer(pagaments, many=True).data
-    
-#     def get_comandes(self, obj):
-#         comandes = Comanda.objects.filter(user=obj.id)
-#         return ComandaSerializer(comandes, many=True).data
-    
-# serializador del modelo comanda
-# class ComandaSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Comanda
-#         fields = ['id', 'carreto', 'data', 'user']
 class ComandaSerializer(serializers.ModelSerializer):
     #Aqui indico que el campo carreto de Comanda tiene qie serializar-lo
     #Usando el serializador de Carrito para mostrar todo el contenido del carrito
