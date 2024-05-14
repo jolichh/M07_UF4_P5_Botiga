863a9a6e18bbb78fa8bec3678b0363978a3fdba4
= <=>
2024-05-14 18:25:32 +0200

    He agregado que se muestre el contenido del carrito

--- botiga_grupJ/comandes/serializers.py
+++ botiga_grupJ/comandes/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+
+from carreto.serializers import CarritoSerializer
 from .models import Comanda
 
 class ComandaSerializer(serializers.ModelSerializer):
+    #Aqui indico que el campo carreto de Comanda tiene qie serializar-lo
+    #Usando el serializador de Carrito para mostrar todo el contenido del carrito
+    carreto = CarritoSerializer(read_only=True)
+
     class Meta:
         model = Comanda
         fields = ['id', 'carreto', 'compra_realizada', 'data', 'user']
+
+
